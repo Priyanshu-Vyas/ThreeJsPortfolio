1f65b4397f9c6cbd479c676737f8328f5ce016c1
priyanshu-vyas <vyaspratik36@gmail.com>
2024-10-20 15:04:00 +0530

    Linking Resume Site to Navbar Name

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto">
         <div className="flex justify-between items-center py-5 mx-auto c-space">
           <a
-            href="/"
+            href="https://priyanshuvyasportfolio.vercel.app/"
             className="text-neutral-400 font-bold text-xl hover:text-white transition-colors"
           >
             PRIYANSHU VYAS
